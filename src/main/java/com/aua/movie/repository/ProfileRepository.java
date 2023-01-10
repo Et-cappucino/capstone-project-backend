338ae1f053bd1ccf949995e4eae03efebd6f9118
Emin Ter-Mkrtchyan <emin.termkrtchyan@gmail.com>
2023-01-10 19:36:07 +0400

    Add data-jpa dependency, repository and model packages with corresponding models and repos

---
+++ src/main/java/com/aua/movie/repository/ProfileRepository.java
@@ -0,0 +1,9 @@
+package com.aua.movie.repository;
+
+import com.aua.movie.model.Profile;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProfileRepository extends JpaRepository<Profile,Long> {
+}
