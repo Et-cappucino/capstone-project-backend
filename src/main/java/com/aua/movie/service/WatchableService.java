8d1a1bf55702e0e71c215de812f831c53263d8c4
Emin Ter-Mkrtchyan <emin.termkrtchyan@gmail.com>
2023-01-12 22:47:37 +0400

    Update Profile and Watchable services

--- src/main/java/com/aua/movie/service/WatchableService.java
+++ src/main/java/com/aua/movie/service/WatchableService.java
@@ -1,4 +1,18 @@
 package com.aua.movie.service;
 
+import com.aua.movie.model.Watchable;
+
+import java.util.List;
+
 public interface WatchableService {
+
+    List<Watchable> findAll();
+
+    Watchable getWatchable(Long id);
+
+    Watchable registerWatchable(Watchable watchable);
+
+    void updateWatchable(Watchable watchable);
+
+    void deleteWatchable(Long id);
 }
--- src/main/java/com/aua/movie/service/impl/WatchableServiceImpl.java
+++ src/main/java/com/aua/movie/service/impl/WatchableServiceImpl.java
@@ -1,13 +1,44 @@
 package com.aua.movie.service.impl;
 
+import com.aua.movie.model.Watchable;
 import com.aua.movie.repository.WatchableRepository;
 import com.aua.movie.service.WatchableService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class WatchableServiceImpl implements WatchableService {
 
     private final WatchableRepository watchableRepository;
+
+    @Override
+    public List<Watchable> findAll() {
+        return watchableRepository.findAll();
+    }
+
+    @Override
+    public Watchable getWatchable(Long id) {
+        return watchableRepository.findById(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+    }
+
+    @Override
+    public Watchable registerWatchable(Watchable watchable) {
+        return watchableRepository.save(watchable);
+    }
+
+    @Override
+    public void updateWatchable(Watchable watchable) {
+        watchableRepository.save(watchable);
+    }
+
+    @Override
+    public void deleteWatchable(Long id) {
+        watchableRepository.deleteById(id);
+    }
 }
