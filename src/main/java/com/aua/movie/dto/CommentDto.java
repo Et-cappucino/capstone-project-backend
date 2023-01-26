1c04cb77ce180a19411cfd7c1487c81c06229e9d
Emin Ter-Mkrtchyan <emin.termkrtchyan@gmail.com>
2023-01-27 00:17:27 +0400

    Create Comment repository and DTO

---
+++ src/main/java/com/aua/movie/dto/CommentDto.java
@@ -0,0 +1,41 @@
+package com.aua.movie.dto;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@ApiModel(description = "Represents an info which is required to create a Comment")
+public class CommentDto {
+
+    @ApiModelProperty(value = "commentId")
+    private Long id;
+
+    @ApiModelProperty(value = "Text of the comment")
+    @NotBlank
+    private String text;
+
+    @ApiModelProperty(value = "Id of the author of the comment")
+    @NotNull
+    @Positive
+    private Long commenterId;
+
+    @ApiModelProperty(value = "Id of the watchable under which the comment is posted")
+    @NotNull
+    @Positive
+    private Long watchableId;
+
+    @ApiModelProperty(value = "The exact time the comment was posted")
+    private LocalDateTime createdAt = LocalDateTime.now();
+}
