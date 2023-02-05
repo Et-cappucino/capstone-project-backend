975553ad56f389141aa896d19005caba8a38eb11
Emin Ter-Mkrtchyan <emin.termkrtchyan@gmail.com>
2023-02-05 21:26:21 +0400

    Add pagination support for Watchables

--- src/main/java/com/aua/movie/repository/WatchableRepository.java
+++ src/main/java/com/aua/movie/repository/WatchableRepository.java
@@ -1,9 +1,29 @@
 package com.aua.movie.repository;
 
 import com.aua.movie.model.Watchable;
+import com.aua.movie.model.enums.WatchableType;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
 
 @Repository
 public interface WatchableRepository extends JpaRepository<Watchable, Long> {
+
+    Page<Watchable> findAllByType(WatchableType type, Pageable pageRequest);
+
+    @Query("SELECT w FROM Watchable w WHERE w.releaseDate > :cutoffDate AND w.releaseDate < :now")
+    Page<Watchable> findAllLatest(@Param("cutoffDate") LocalDate cutoffDate,
+                                  @Param("now") LocalDate now,
+                                  Pageable pageRequest);
+
+    @Query("SELECT w FROM Watchable w WHERE w.rating >= :minRating")
+    Page<Watchable> findAllPopular(@Param("minRating") double minRating, Pageable pageRequest);
+
+    @Query("SELECT w FROM Watchable w WHERE w.releaseDate > :now")
+    Page<Watchable> findAllUpcoming(@Param("now") LocalDate now, Pageable pageRequest);
 }
