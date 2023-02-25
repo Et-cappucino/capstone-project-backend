177918a4370bf43c382f95331f5ea9bc2e263537
Emin Ter-Mkrtchyan <emin.termkrtchyan@gmail.com>
2023-02-25 16:37:08 +0400

    Add views for email confirmation failure cases, and a separate EmailVerificationExceptionHandler class to provide the views for corresponding exceptions

--- src/main/java/com/aua/movie/exception/GlobalControllerExceptionHandler.java
+++ src/main/java/com/aua/movie/exception/GlobalControllerExceptionHandler.java
@@ -10,9 +10,7 @@ import java.time.ZonedDateTime;
 
 import static org.springframework.http.HttpStatus.BAD_REQUEST;
 import static org.springframework.http.HttpStatus.FORBIDDEN;
-import static org.springframework.http.HttpStatus.GONE;
 import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
-import static org.springframework.http.HttpStatus.NOT_FOUND;
 
 @RestControllerAdvice
 public class GlobalControllerExceptionHandler {
@@ -35,36 +33,6 @@ public class GlobalControllerExceptionHandler {
                 ZonedDateTime.now(ZoneId.of("Z"))
         );
         return ResponseEntity.status(FORBIDDEN).body(exceptionDetails);
-    }
-
-    @ExceptionHandler(ConfirmationTokenNotFoundException.class)
-    public ResponseEntity<Object> handleConfirmationTokenNotFoundException(ConfirmationTokenNotFoundException exception) {
-        ExceptionDetails exceptionDetails = new ExceptionDetails(
-                exception.getMessage(),
-                exception.getClass().getSimpleName(),
-                ZonedDateTime.now(ZoneId.of("Z"))
-        );
-        return ResponseEntity.status(NOT_FOUND).body(exceptionDetails);
-    }
-
-    @ExceptionHandler(EmailAlreadyConfirmedException.class)
-    public ResponseEntity<Object> handleEmailAlreadyConfirmedException(EmailAlreadyConfirmedException exception) {
-        ExceptionDetails exceptionDetails = new ExceptionDetails(
-                exception.getMessage(),
-                exception.getClass().getSimpleName(),
-                ZonedDateTime.now(ZoneId.of("Z"))
-        );
-        return ResponseEntity.status(GONE).body(exceptionDetails);
-    }
-
-    @ExceptionHandler(ConfirmationTokenExpiredException.class)
-    public ResponseEntity<Object> handleConfirmationTokenExpiredException(ConfirmationTokenExpiredException exception) {
-        ExceptionDetails exceptionDetails = new ExceptionDetails(
-                exception.getMessage(),
-                exception.getClass().getSimpleName(),
-                ZonedDateTime.now(ZoneId.of("Z"))
-        );
-        return ResponseEntity.status(GONE).body(exceptionDetails);
     }
 
     @ExceptionHandler(DataIntegrityViolationException.class)
