907c1b3acd803b04a1b6934f0a9b1b6bf8d6d59e
Emin Ter-Mkrtchyan <emin.termkrtchyan@gmail.com>
2023-04-28 20:51:43 +0400

    Update Recommendation controller and service classes to recommend watchables based on content

--- src/main/java/com/aua/movie/controller/RecommendationController.java
+++ src/main/java/com/aua/movie/controller/RecommendationController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import static com.aua.movie.model.enums.RecommendationType.CONTENT_BASED;
 import static com.aua.movie.model.enums.RecommendationType.GENRE_BASED;
 
 @Api(value = "Recommendation service rest API")
@@ -32,6 +33,16 @@ public class RecommendationController {
                                                                               @RequestParam(value = "pageSize", required = false, defaultValue = "5") Integer pageSize,
                                                                               @RequestParam(value = "watchableId") Long watchableId) {
         Page<WatchableDto> body = recommendationService.findAllRecommended(GENRE_BASED, watchableId, PageRequest.of(pageNumber,pageSize));
+        return ResponseEntity.ok(body);
+    }
+
+    @ApiOperation(value = "Get recommended Watchables based on content of the provided watchable with pagination support", tags = "recommendation-controller")
+    @ApiResponses(value = {@ApiResponse(code = 200, message = "OK"), @ApiResponse(code = 400, message = "Bad Request")})
+    @GetMapping("/content_based")
+    public ResponseEntity<Page<WatchableDto>> recommendWatchablesBasedOnContent(@RequestParam(value = "pageNumber", required = false, defaultValue = "0") Integer pageNumber,
+                                                                                @RequestParam(value = "pageSize", required = false, defaultValue = "5") Integer pageSize,
+                                                                                @RequestParam(value = "watchableId") Long watchableId) {
+        Page<WatchableDto> body = recommendationService.findAllRecommended(CONTENT_BASED, watchableId, PageRequest.of(pageNumber,pageSize));
         return ResponseEntity.ok(body);
     }
 }
--- src/main/java/com/aua/movie/service/impl/RecommendationServiceImpl.java
+++ src/main/java/com/aua/movie/service/impl/RecommendationServiceImpl.java
@@ -27,6 +27,8 @@ import java.util.Map;
 import java.util.Objects;
 import java.util.stream.Collectors;
 
+import static com.aua.movie.model.enums.RecommendationType.GENRE_BASED;
+
 @Service
 @RequiredArgsConstructor
 public class RecommendationServiceImpl implements RecommendationService {
@@ -52,7 +54,8 @@ public class RecommendationServiceImpl implements RecommendationService {
         Watchable watchable = watchableRepository.findById(watchableId)
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
         RestTemplate restTemplate = new RestTemplate();
-        Integer[] ids = restTemplate.postForObject(url, payload(watchable.getGenres()), Integer[].class);
+        Object request = (type == GENRE_BASED) ? payload(watchable.getGenres()) : payload(watchable.getName());
+        Integer[] ids = restTemplate.postForObject(url + type.getPath(), request, Integer[].class);
         List<Integer> idsAsList = Arrays.asList(Objects.requireNonNull(ids));
         return idsAsList.stream()
                 .mapToLong(Integer::longValue)
@@ -62,6 +65,13 @@ public class RecommendationServiceImpl implements RecommendationService {
 
     private HttpEntity<Map<String, Object>> payload(List<Genre> genres) {
         Map<String, Object> requestBody = Map.of("genres", genres);
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        return new HttpEntity<>(requestBody, headers);
+    }
+
+    private HttpEntity<Map<String, Object>> payload(String title) {
+        Map<String, Object> requestBody = Map.of("title", title);
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
         return new HttpEntity<>(requestBody, headers);
